7b57d77629f55761a857e7a810e820fa8e1d16a9
Darrien Huntley <info@dhuntleyinc.com>
2020-12-29 10:23:01 -0500

    Initial Commit

---
+++ GitHubReadyProject/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  GitHubReadyProject
+//
+//  Created by Darrien Huntley on 12/29/20.
+//
+
+// Make sure Create Git Repository is not checked
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Make sure Create Git Repository is not checked ")
+
+            Text("preferances | + | GitHub | Signin")
+            
+            Text("Source Control | Create ReGit Repository")
+
+            
+            
+            
+        }
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubReadyProject/GitHubReadyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReadyProjectApp.swift
+//  GitHubReadyProject
+//
+//  Created by Darrien Huntley on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReadyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
